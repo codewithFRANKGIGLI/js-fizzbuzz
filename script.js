90726bab42b7517d6b0636475e6c8c8c2f158741
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-07 14:42:43 +0100

    feat: 1 step

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 // ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+for(let i = 1; i <= 100; i++) {
+    // console.log(i);
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log('fizzbuzz');
+    } else if(i % 3 == 0) {
+        console.log('fizz');
+    } else if(i % 5 == 0) {
+        console.log('buzz');
+    } else {
+        console.log(i);
+    }
+}
