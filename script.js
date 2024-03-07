1378a22f548862d6f04cbf95dbe4c26ad8d8b69f
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-07 14:46:58 +0100

    feat: container dom

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ for(let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare. FATTO
+
+
+
+
+
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
